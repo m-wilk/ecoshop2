9a2e24ba800cfdd1c54b9a469d3932045f2152e6
m-wilk <magdalenawilk86@gmail.com>
2024-07-16 13:59:49 +0200

    ddd

--- app/src/ui/organism/hero-section/HeroSection.js
+++ app/src/ui/organism/hero-section/HeroSection.js
@@ -3,6 +3,11 @@ import HeroImg1 from "../../../assets/img/hero-img-1.png";
 import HeroImg2 from "../../../assets/img/hero-img-2.png";
 import HeroImg3 from "../../../assets/img/hero-img-3.png";
 import Slider from "react-slick";
+import { ReactComponent as FreeShipping } from "../../../assets/img/free-shipping.svg";
+import { ReactComponent as FreeReturn } from "../../../assets/img/free-return.svg";
+import { ReactComponent as SecurePayment } from "../../../assets/img/secure-payment.svg";
+import { ReactComponent as BestQuality } from "../../../assets/img/best-quality.svg";
+
 
 const settings = {
   dots: true,
@@ -70,6 +75,46 @@ const HeroSection = () => {
             </div>
           </div>
         </Slider>
+        <div className="bg-primary rounded-1 text-white es-hero-service">
+          <div className="row">
+            <div className="col-lg-3 col-sm-6">
+              <div className="d-flex align-items-center gap-3">
+                <FreeShipping />
+                <div>
+                  <h5 className="fw-bold m-0">Free Shipping</h5>
+                  <p className="m-0">When ordering over $100</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="d-flex align-items-center gap-3">
+                <FreeReturn />
+                <div>
+                  <h5 className="fw-bold m-0">Free Return</h5>
+                  <p className="m-0">When ordering over $100</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="d-flex align-items-center gap-3">
+                <SecurePayment />
+                <div>
+                  <h5 className="fw-bold m-0">Secure Payment</h5>
+                  <p className="m-0">When ordering over $100</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6">
+              <div className="d-flex align-items-center gap-3">
+                <BestQuality />
+                <div>
+                  <h5 className="fw-bold m-0">Best Quality</h5>
+                  <p className="m-0">When ordering over $100</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
